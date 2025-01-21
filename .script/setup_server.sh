c24b85e950c7e95f16c5fd85582deb67ead11186
Dmitriy <d.cherenkov01@gmail.com>
2025-01-21 17:53:12 +0300

    ++

--- .script/setup_server.sh
+++ .script/setup_server.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 sudo apt update 
-sudo apt upgrade
+sudo apt upgrade -y
+
+git clone https://github.com/DmitriyChe01/dotfiles_server.git 
+cp -r dotfiles_server/ ./
+rm -r dotfiles_server/
+
 sudo apt install -y gcc make git neovim curl fish ranger htop tmux 
 
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
-
+chsh -s /usr/bin/fish
 
